4a992be1d17816fc2e42389476d7fd423b94a6d7
Mohammad Alavi <alavi_hassan@yahoo.com>
2021-07-10 22:11:50 +0200

    init

---
+++ contracts/Apple.sol
@@ -0,0 +1,10 @@
+pragma solidity =0.5.16;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/ERC20Detailed.sol";
+
+contract Apple is ERC20, ERC20Detailed{
+    constructor(uint256 initialSupply) ERC20Detailed("Apple", "APPL", 18) public {
+        _mint(msg.sender, initialSupply);
+    }
+}
---
+++ migrations/02_deploy_contracts.js
@@ -0,0 +1,20 @@
+const Factory = artifacts.require("UniswapV2Factory.sol");
+const Apple = artifacts.require("Apple.sol")
+const Pear = artifacts.require("Pear.sol")
+
+module.exports = async function (deployer, network, addresses) {
+  await deployer.deploy(Factory, '0xC40bb332cD6cB4E1f195379b0E04283e6DB97f7d');
+  const factory = await Factory.deployed();
+  if (network === 'mainnet') {
+    token1Address = '';
+    token2Address = '';
+  } else {
+    await deployer.deploy(Apple, 10000000000000000000000);
+    await deployer.deploy(Pear, 10000000000000000000000);
+    const apple = await Apple.deployed();
+    const pear = await Pear.deployed();
+    appleAddress = apple.address;
+    pearAddress = pear.address;
+  }
+  await factory.createPair(appleAddress, pearAddress);
+};
